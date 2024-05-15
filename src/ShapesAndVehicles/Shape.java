976ead621f55b500c923f476275a6e317fdd1ddd
IMGradeE <wilkespayton@gmail.com>
2024-05-15 11:35:14 -0600

    Completed Learning Activity 3: Inheritance, Interface and Polymorphism (Package: ShapesAndVehicles)

--- src/ShapesAndVehicles/Shape.java
+++ src/ShapesAndVehicles/Shape.java
@@ -2,15 +2,17 @@ package ShapesAndVehicles;
 
 public abstract class Shape {
     public abstract double calculateArea();
+    protected double radius;
+    protected double height;
+    protected double width;
 }
 
 class Circle extends Shape{
-    private double radius;
     public Circle(double r) throws IllegalArgumentException{
-        if (r < 0) {
+        if (radius < 0) {
             throw new IllegalArgumentException("Circle cannot be instantiated with a negative radius.");
         }
-        radius = r;
+        this.radius = r;
     }
     @Override
     public double calculateArea() {
@@ -19,22 +21,19 @@ class Circle extends Shape{
 }
 
 class Rectangle extends Shape implements Resizable {
-    private double length;
-    private double width;
-
-    public Rectangle(double w, double l) throws IllegalArgumentException{
+    public Rectangle(double w, double h) throws IllegalArgumentException{
         if (w < 0) {
             throw new IllegalArgumentException("Rectangle cannot be instantiated with a negative width.");
-        }else if (l < 0) {
+        }else if (h < 0) {
             throw new IllegalArgumentException("Rectangle cannot be instantiated with a negative length.");
         }
-        width = w;
-        length = l;
+        this.width = w;
+        this.height = h;
     }
 
     @Override
     public double calculateArea(){
-        return width*length;
+        return width*height;
     }
 
     @Override
@@ -43,27 +42,24 @@ class Rectangle extends Shape implements Resizable {
             throw new IllegalArgumentException("Rectangle cannot be resized with a negative scalar.");
         }
         width *= scalar;
-        length *= scalar;
+        height *= scalar;
     }
 }
 
 class Triangle extends Shape {
-    private double height;
-    private double base;
-
-    public Triangle(double b, double h) throws IllegalArgumentException{
-        if (h < 0) {
-            throw new IllegalArgumentException("Triangle cannot be instantiated with a negative height");
-        }else if (b < 0) {
-            throw new IllegalArgumentException("Triangle cannot be instantiated with a negative base.");
-        }
-        height= h;
-        base = b;
+        public Triangle(double b, double h) throws IllegalArgumentException{
+            if (h < 0) {
+                throw new IllegalArgumentException("Triangle cannot be instantiated with a negative height");
+            }else if (b < 0) {
+                throw new IllegalArgumentException("Triangle cannot be instantiated with a negative base.");
+            }
+            this.width = b;
+            this.height = h;
     }
 
     @Override
     public double calculateArea(){
-        return (base*height)/2.0;
+        return (width*height)/2.0;
     }
 }
 
