09d5065ca94bfdeb9dcfffdab1662907c7eb3617
IMGradeE <wilkespayton@gmail.com>
2024-05-15 11:42:41 -0600

    Compulsively refactored Learning Activity 3: Inheritance, Interface and Polymorphism (Package: ShapesAndVehicles)

--- src/ShapesAndVehicles/Vehicle.java
+++ src/ShapesAndVehicles/Vehicle.java
@@ -6,7 +6,7 @@ import java.security.InvalidParameterException;
 // checking if years is a key to the value of the make + model in an Entry<year,make+model>
 // I think this would be inefficient as described but could be optimized well
 public abstract class Vehicle implements Driveable {
-    public Vehicle(String make, String model, int year) throws Exception {
+    public Vehicle(String make, String model, int year) throws InvalidParameterException {
         if (make == null || make.isEmpty() || model == null || model.isEmpty()) {
             throw new InvalidParameterException("Make or Model cannot be empty");
         }else if (year < MIN_YEAR || year > MAX_YEAR) {
@@ -32,18 +32,18 @@ public abstract class Vehicle implements Driveable {
     public void drive() {
         if (this.driving) {
             this.speed = 0.00;
-            System.out.println(String.format("The %d %s %s has stopped.", year, make, model));
+            System.out.printf("The %d %s %s has stopped.%n", year, make, model);
         }else{
             this.speed = 15.00;
-            System.out.println(String.format("The %d %s %s is driving.", year, make, model));
+            System.out.printf("The %d %s %s is driving.%n", year, make, model);
         }
         this.driving = !this.driving;
     }
     protected void displayInfo(){
-        System.out.println(String.format("Make: %s\nModel: %s\nYear: %d\n",make,model,year));
+        System.out.printf("Make: %s\nModel: %s\nYear: %d\n%n",make,model,year);
     }
     protected void displaySpeed(){
-        System.out.println(String.format("Current Speed: %d km/h" ,(int) speed));
+        System.out.printf("Current Speed: %d km/h%n",(int) speed);
     }
     protected void accelerate(){
         if (!this.driving){
@@ -51,10 +51,10 @@ public abstract class Vehicle implements Driveable {
         }else{
             if (this.speed <= MAX_SPEED_KM - SPEED_INCREMENT_KM){
                 this.speed += SPEED_INCREMENT_KM;
-                System.out.println(String.format("The %d %s %s is accelerating.", year, make, model));
+                System.out.printf("The %d %s %s is accelerating.%n", year, make, model);
             }else {
                 this.speed = MAX_SPEED_KM;
-                System.out.println(String.format("The %d %s %s has reached its top speed!", year, make, model));
+                System.out.printf("The %d %s %s has reached its top speed!%n", year, make, model);
             }
         }
     }
@@ -64,7 +64,7 @@ public abstract class Vehicle implements Driveable {
         }else{
             if(this.speed >= SPEED_DECREMENT_KM){
                 this.speed -= 10.0;
-                System.out.println(String.format("The %d %s %s is decelerating.", year, make, model));
+                System.out.printf("The %d %s %s is decelerating.%n", year, make, model);
             }else {
                 drive();
             }
@@ -78,7 +78,7 @@ public abstract class Vehicle implements Driveable {
 }
 
 class Car extends Vehicle{
-    public Car(String make, String model, int year) throws Exception {
+    public Car(String make, String model, int year) {
         super(make, model, year);
         // subclass specific checks for make/model could be made here if desired.
         // Any other initialization actions specific to this subtype can also be made here.
@@ -89,29 +89,29 @@ class Car extends Vehicle{
 }
 
 class Motorcycle extends Vehicle {
-    public Motorcycle(String make, String model, int year) throws Exception {
+    public Motorcycle(String make, String model, int year) {
         super(make, model, year);
     }
     // methods
     public void wheelie(){
         if(this.driving){
-            System.out.println(String.format("The %d %s %s is doing a wheelie!", year, make, model)); //
+            System.out.printf("The %d %s %s is doing a wheelie!%n", year, make, model); //
         }
     }
     public void rollStoppie(){
         if(this.driving && this.speed >= 30){
-            System.out.println(String.format("The %d %s %s is rolling a stoppie!", year, make, model)); //
+            System.out.printf("The %d %s %s is rolling a stoppie!%n", year, make, model); //
             this.speed = this.speed/2;
         }else {
-            System.out.println(String.format("The %d %s %s is going too slow to roll a stoppie.", year, make, model));
+            System.out.printf("The %d %s %s is going too slow to roll a stoppie.%n", year, make, model);
         }
     }
     public void stoppie(){
         if(this.driving && this.speed >= 30){
-            System.out.println(String.format("The %d %s %s is doing a stoppie!", year, make, model)); //
+            System.out.printf("The %d %s %s is doing a stoppie!%n", year, make, model); //
             this.speed = 0;
         }else {
-            System.out.println(String.format("The %d %s %s is going too slow to do a stoppie.", year, make, model));
+            System.out.printf("The %d %s %s is going too slow to do a stoppie.%n", year, make, model);
         }
     }
     protected void displaySpeed(){
